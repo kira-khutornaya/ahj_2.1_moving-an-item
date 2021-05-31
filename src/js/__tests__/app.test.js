b4b5efdb80b8364ff8977b47d5902a4db50eda6f
Kira Khutornaya <kira.khutornaya@mail.ru>
2021-05-31 22:47:21 +0300

    tests updated

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -10,9 +10,22 @@ describe('GameBoard:', () => {
 });
 
 describe('GamePlay:', () => {
-  test('should draw a new game board', () => {
-    new GamePlay().drawGameBoard();
+  const game = new GamePlay();
+  game.init();
+  test('init method should draw a new game board', () => {
     const holes = document.querySelectorAll('.hole');
     expect(holes.length).toBe(16);
+  });
+
+  test('activateHole (deactivateHole) method should add (remove) class to the index', () => {
+    game.activateHole(3);
+    expect(game.holes[3].classList.contains('hole_has-goblin')).toBeTruthy();
+    game.deactivateHole(3);
+    expect(game.holes[3].classList.contains('hole_has-goblin')).toBeFalsy();
+  });
+
+  test('getPosition method should return random number', () => {
+    const random = new GamePlay().getPosition();
+    expect(random).toBeTruthy();
   });
 });
